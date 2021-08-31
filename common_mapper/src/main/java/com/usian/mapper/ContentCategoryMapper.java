c79a2e4633fb2dc152556f5997502d8264f84e83
王志文 <1447279309@qq.com>
2021-08-31 19:42:07 +0800

    asd

---
+++ common_mapper/src/main/java/com/usian/mapper/ContentCategoryMapper.java
@@ -0,0 +1,16 @@
+package com.usian.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.usian.pojo.ContentCategory;
+
+/**
+ * <p>
+ * 内容分类 Mapper 接口
+ * </p>
+ *
+ * @author 韩丛
+ * @since 2021-08-06
+ */
+public interface ContentCategoryMapper extends BaseMapper<ContentCategory> {
+
+}
